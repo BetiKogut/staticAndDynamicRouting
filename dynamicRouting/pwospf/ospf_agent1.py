c5368c821618a9b931d8316dd865a106324dd0b8
PiotrPosiadala <pioposiadala@gmail.com>
2020-04-23 23:05:36 +0200

    LSUPD prototyping

--- dynamicRouting/pwospf/ospf_agent1.py
+++ dynamicRouting/pwospf/ospf_agent1.py
@@ -10,13 +10,13 @@ import dijkstra
 ###---------------------------------###
 OSPF_VERSION = "2"
 AREA_ID = "0.0.0.0"
-LSUINT = 10
+LSUINT = 30
 HELLOINT = 10
 NEIGHBOR_TIMEOUT = 3*HELLOINT
 ROUTER_INTERFACES =[
-    { "name": "s1-eth0",    "address":"192.168.1.1",     "mask":"24",   "mac":"00:00:00:00:01:00",  "neighbor_ip":"192.168.1.2"},
-    { "name": "s1-eth1",    "address":"192.168.11.1",    "mask":"24",   "mac":"00:00:00:00:01:01",  "neighbor_ip":"192.168.11.2"},
-    { "name": "s1-eth2",    "address":"192.168.12.1",    "mask":"24",   "mac":"00:00:00:00:01:02",  "neighbor_ip":"192.168.12.2"}]
+    { "name": "s1-eth0",    "address":"192.168.1.1",     "mask":"24",   "subnet":"192.168.1.0",   "mac":"00:00:00:00:01:00",  "neighbor_ip":"192.168.1.2"},
+    { "name": "s1-eth1",    "address":"192.168.11.1",    "mask":"24",   "subnet":"192.168.11.0",   "mac":"00:00:00:00:01:01",  "neighbor_ip":"192.168.11.2"},
+    { "name": "s1-eth2",    "address":"192.168.12.1",    "mask":"24",   "subnet":"192.168.12.0",   "mac":"00:00:00:00:01:02",  "neighbor_ip":"192.168.12.2"}]
 ROUTER_CP_INTERFACE = {"name": "s1-eth4",    "address":"192.168.101.2",     "mask":"24",   "mac":"00:00:00:00:01:04"}
 ROUTER_ID = ROUTER_INTERFACES[0].get("address")
 ALLSPFRouters = "224.0.0.5"
@@ -29,9 +29,12 @@ def log(msg):
     print("{}   {}".format(time.strftime('%H:%M:%S'),msg))
 
 class OspfInterface:
-    def __init__(self, ip_address, mask = "24", helloint = HELLOINT, neighbor_ip = "0", neighbor_id = "0"):
+    def __init__(self, name, ip_address, mac, subnet, mask = "24",  helloint = HELLOINT, neighbor_ip = "0", neighbor_id = "0"):
+        self.name = name
         self.ip_address = ip_address
+        self.mac = mac
         self.mask = mask
+        self.subnet = subnet
         self.helloint = helloint
         self.neighbor_ip = neighbor_ip
         self.neighbor_id = neighbor_id
@@ -44,8 +47,11 @@ class RouterClass:
     def __init__(self, router_interfaces):
         for interface in router_interfaces:
             self.ospf_interfaces.append(OspfInterface(
+                name = interface["name"],
                 ip_address = interface["address"],
+                mac = interface["mac"],
                 mask = interface["mask"],
+                subnet = interface["subnet"],
                 helloint = HELLOINT,
                 neighbor_ip = "0",
                 neighbor_id = "0"))
@@ -62,9 +68,10 @@ class DatabaseClass(list):
         self.pop(index_to_remove)
 
 class OspfHelloThread(threading.Thread):
-    def __init__(self,*args, **kwargs):
+    def __init__(self, router, *args, **kwargs):
         super(OspfHelloThread, self).__init__(*args, **kwargs)
         self._stop_event = threading.Event()
+        self.router = router
     def run(self):
         while(True):
             if self.stopped():
@@ -75,13 +82,46 @@ class OspfHelloThread(threading.Thread):
         self._stop_event.set()
     def stopped(self):
         return self._stop_event.is_set()
-    def generate_hello(self):
-        for i in ROUTER_INTERFACES:
-            p = scapy_ospf.Ether(src = ROUTER_CP_INTERFACE.get("mac"), dst = i.get("mac"))/IP(src = i.get("address"), dst = i.get("neighbor_ip"))/scapy_ospf.OSPF_Hdr(src=ROUTER_ID,area=AREA_ID)/scapy_ospf.OSPF_Hello()
+    def generate_hello(self): #TODO
+        for i in self.router.ospf_interfaces:
+            p = scapy_ospf.Ether(src = ROUTER_CP_INTERFACE.get("mac"), dst = i.mac)/IP(src = i.ip_address, dst = i.neighbor_ip)/scapy_ospf.OSPF_Hdr(src=ROUTER_ID,area=AREA_ID)/scapy_ospf.OSPF_Hello()
             log("Hello generated")
             p.show()
-            sendp(p, iface=i.get("name"))
+            sendp(p, iface=i.name)
             log("Hello sent")
+            
+class OspfLSUThread(threading.Thread):
+    def __init__(self, router, *args, **kwargs):
+            super(OspfLSUThread, self).__init__(*args, **kwargs)
+            self._stop_event = threading.Event()
+            self.router = router
+    def run(self):
+        time.sleep(3)
+        while(True):
+            if self.stopped():
+                return
+            self.generate_lsu()
+            time.sleep(LSUINT)
+    def stop(self):
+        self._stop_event.set()
+    def stopped(self):
+        return self._stop_event.is_set()
+    def generate_lsu(self):
+            for i in self.router.ospf_interfaces: #TODO
+                p = scapy_ospf.Ether(src = ROUTER_CP_INTERFACE.get("mac"), dst = i.mac)/IP(src = i.ip_address, dst = i.neighbor_ip)/scapy_ospf.OSPF_Hdr(src=ROUTER_ID,area=AREA_ID)/scapy_ospf.OSPF_LSUpd()
+                log("LSU generated")
+                for k in self.router.ospf_interfaces:
+                    if k is not i:
+                        if k.mask == "24":
+                            temp_mask = "255.255.255.0"
+                        else
+                            temp_mask = mask
+                        p[scapy_ospf.OSPF_LSUpd].lsalist.append(scapy_ospf.OSPF_Network_LSA(id=k.subnet,adrouter=self.router.router_id, mask = k.mask))
+                        log("LSA Appended")
+                p.show()
+                sendp(p, iface=i.name)
+                log("LSU sent")
+
 
 class OspfSnifferThread(threading.Thread):
     def __init__(self, Database, *args, **kwargs):
@@ -116,25 +156,6 @@ class OspfSnifferThread(threading.Thread):
         if( packet[scapy_ospf.OSPF_Hdr].version == OSPF_VERSION and packet[scapy_ospf.OSPF_Hdr].area == AREA_ID and packet[scapy_ospf.OSPF_Hdr].authtype == AUTH_TYPE):
             is_packet_valid = False
         return is_packet_valid
-        
-def generate_lsu(    
-    eth_src = ROUTER_CP_INTERFACE.get("mac"), 
-    eth_dst = ROUTER_CP_INTERFACE.get("mac"), 
-    ip_src =  ROUTER_CP_INTERFACE.get("address"),
-    ip_dst =  ALLSPFRouters):
-    #TODO tutaj dst nie all routers
-    p = scapy_ospf.Ether(src = eth_src, dst = eth_dst)/IP(src = ip_src, dst = ip_dst )/scapy_ospf.OSPF_Hdr(src=ROUTER_ID,area=AREA_ID)/scapy_ospf.OSPF_LSUpd()
-    return p
-
-def generate_OSPF_Interfaces():
-    OSPF_Interfaces = []
-    for interface in ROUTER_INTERFACES:
-        OSPF_Interfaces.append(OspfInterface(
-            ip_address = interface["address"],
-            mask = interface["mask"],
-            helloint = HELLOINT))
-        print("{}: OSPF Interface = {} generated!".format(time.strftime('%H:%M:%S'), interface["name"]))
-    return OSPF_Interfaces
 
 ###---------------------------------###
 ### ----------- RUNNING ----------- ###
@@ -144,9 +165,12 @@ def main():
     Router = RouterClass(ROUTER_INTERFACES)
     Database_Topo = DatabaseClass()
     
-    OspfHello = OspfHelloThread()
+    OspfHello = OspfHelloThread(Router)
+    OspfLSU = OspfLSUThread(Router)
     OspfHello.start()
     print("{} OSPF_Hello_Thread started!".format(time.strftime('%H:%M:%S')))
+    OspfLSU.start()
+    print("{} OSPF_LSU_Thread started!".format(time.strftime('%H:%M:%S')))
     #OspfSniffer = OspfSnifferThread()
     #OspfSniffer.start()
     #log("OSPF_Sniffer_Thread started!")
@@ -155,6 +179,8 @@ def main():
 
     OspfHello.stop()
     OspfHello.join()
+    OspfLSU.stop()
+    OspfLSU.join()
     #OspfSniffer.stop()
     #OspfSniffer.join()
     
--- dynamicRouting/pwospf/ospf_agent2.py
+++ dynamicRouting/pwospf/ospf_agent2.py
@@ -10,14 +10,14 @@ import dijkstra
 ###---------------------------------###
 OSPF_VERSION = "2"
 AREA_ID = "0.0.0.0"
-LSUINT = 10
+LSUINT = 30
 HELLOINT = 10
 NEIGHBOR_TIMEOUT = 3*HELLOINT
 ROUTER_INTERFACES =[
-    { "name": "s2-eth0",    "address":"192.168.2.1",     "mask":"24",   "mac":"00:00:00:00:02:00",  "neighbor_ip":"192.168.2.2"},
-    { "name": "s2-eth1",    "address":"192.168.11.2",    "mask":"24",   "mac":"00:00:00:00:02:01",  "neighbor_ip":"192.168.11.1"},
-    { "name": "s2-eth2",    "address":"192.168.13.2",    "mask":"24",   "mac":"00:00:00:00:02:02",  "neighbor_ip":"192.168.13.1"},
-    { "name": "s2-eth3",    "address":"192.168.14.1",    "mask":"24",   "mac":"00:00:00:00:02:03",  "neighbor_ip":"192.168.14.2"}]
+    { "name": "s2-eth0",    "address":"192.168.2.1",     "mask":"24",   "subnet":"192.168.2.0",   "mac":"00:00:00:00:02:00",  "neighbor_ip":"192.168.2.2"},
+    { "name": "s2-eth1",    "address":"192.168.11.2",    "mask":"24",   "subnet":"192.168.11.0",   "mac":"00:00:00:00:02:01",  "neighbor_ip":"192.168.11.1"},
+    { "name": "s2-eth2",    "address":"192.168.13.2",    "mask":"24",   "subnet":"192.168.13.0",   "mac":"00:00:00:00:02:02",  "neighbor_ip":"192.168.13.1"},
+    { "name": "s2-eth3",    "address":"192.168.14.1",    "mask":"24",   "subnet":"192.168.14.0",   "mac":"00:00:00:00:02:03",  "neighbor_ip":"192.168.14.2"}]
 ROUTER_CP_INTERFACE = {"name": "s2-eth4",    "address":"192.168.102.2",     "mask":"24",   "mac":"00:00:00:00:02:04"}
 ROUTER_ID = ROUTER_INTERFACES[0].get("address")
 ALLSPFRouters = "224.0.0.5"
@@ -30,9 +30,10 @@ def log(msg):
     print("{}   {}".format(time.strftime('%H:%M:%S'),msg))
 
 class OspfInterface:
-    def __init__(self, ip_address, mask = "24", helloint = HELLOINT, neighbor_ip = "0", neighbor_id = "0"):
+    def __init__(self, ip_address, subnet, mask = "24", helloint = HELLOINT, neighbor_ip = "0", neighbor_id = "0"):
         self.ip_address = ip_address
         self.mask = mask
+        self.subnet = subnet
         self.helloint = helloint
         self.neighbor_ip = neighbor_ip
         self.neighbor_id = neighbor_id
@@ -47,12 +48,12 @@ class RouterClass:
             self.ospf_interfaces.append(OspfInterface(
                 ip_address = interface["address"],
                 mask = interface["mask"],
+                subnet = interface["subnet"],
                 helloint = HELLOINT,
                 neighbor_ip = "0",
                 neighbor_id = "0"))
             print("{}: OSPF Interface = {} generated!".format(time.strftime('%H:%M:%S'), interface["name"]))
         
-
 class DatabaseClass(list):
     def insert(self, hello):
         log("Database insert called")
@@ -77,13 +78,36 @@ class OspfHelloThread(threading.Thread):
         self._stop_event.set()
     def stopped(self):
         return self._stop_event.is_set()
-    def generate_hello(self):
+    def generate_hello(self): #TODO
         for i in ROUTER_INTERFACES:
             p = scapy_ospf.Ether(src = ROUTER_CP_INTERFACE.get("mac"), dst = i.get("mac"))/IP(src = i.get("address"), dst = i.get("neighbor_ip"))/scapy_ospf.OSPF_Hdr(src=ROUTER_ID,area=AREA_ID)/scapy_ospf.OSPF_Hello()
             log("Hello generated")
             p.show()
             sendp(p, iface=i.get("name"))
             log("Hello sent")
+
+class OspfLSUThread(threading.Thread):
+    def __init__(self, *args, **kwargs):
+            super(OspfLSUThread, self).__init__(*args, **kwargs)
+            self._stop_event = threading.Event()
+    def run(self):
+        time.sleep(3)
+        while(True):
+            if self.stopped():
+                return
+            self.generate_lsu()
+            time.sleep(LSUINT)
+    def stop(self):
+        self._stop_event.set()
+    def stopped(self):
+        return self._stop_event.is_set()
+    def generate_lsu(self):
+            for i in ROUTER_INTERFACES: #TODO
+                p = scapy_ospf.Ether(src = ROUTER_CP_INTERFACE.get("mac"), dst = i.get("mac"))/IP(src = i.get("address"), dst = i.get("neighbor_ip"))/scapy_ospf.OSPF_Hdr(src=ROUTER_ID,area=AREA_ID)/scapy_ospf.OSPF_LSUpd()
+                log("LSU generated")
+                p.show()
+                sendp(p, iface=i.get("name"))
+                log("LSU sent")
 
 class OspfSnifferThread(threading.Thread):  
     def __init__(self, Database, *args, **kwargs):
@@ -106,11 +130,23 @@ class OspfSnifferThread(threading.Thread):
         if(self.validate_packet(packet)):
             log("Packet Validated")
             packet.show()
+            msg_type = packet[scapy_ospf.OSPF_Hdr].type
+            log("Msg_type = {}".format(msg_type))
             msg_router_id = packet[scapy_ospf.OSPF_Hdr].src
-            msg_hello_int = packet[scapy_ospf.OSPF_Hello].hellointerval
-            msg = {"router_id" : msg_router_id, "hello_int" : msg_hello_int}
-            self.Database_Topo.insert(msg)
-            print(msg)
+            if msg_type == 1:                       #from scapy_ospf
+                log("Handling hello packet")
+                msg_hello_int = packet[scapy_ospf.OSPF_Hello].hellointerval
+                msg = {"router_id" : msg_router_id, "hello_int" : msg_hello_int}
+                self.Database_Topo.insert(msg)
+                print(msg)
+            elif msg_type == 4:                     #from scapy_ospf
+                log("Handling LSU packet")
+                for lsa in packet[scapy_ospf.OSPF_LSUpd].lsalist:
+                    msg_id = lsa.id
+                    msg_adrouter = lsa.adrouter
+                    msg_mask = lsa.mask
+                    log("link_id = {}    adrouter = {}   mask = {}".format(msg_id, msg_adrouter, msg_mask))
+                    #TODO wiadomosc sparsowana - wrzuc ja teraz do bazy
             
         #packets.append(packet)
     def validate_packet(self, packet):
@@ -118,17 +154,6 @@ class OspfSnifferThread(threading.Thread):
         if( packet[scapy_ospf.OSPF_Hdr].version == OSPF_VERSION and packet[scapy_ospf.OSPF_Hdr].area == AREA_ID and packet[scapy_ospf.OSPF_Hdr].authtype == AUTH_TYPE):
             is_packet_valid = False
         return is_packet_valid
-
-def generate_lsu(    
-    eth_src = ROUTER_CP_INTERFACE.get("mac"), 
-    eth_dst = ROUTER_CP_INTERFACE.get("mac"), 
-    ip_src =  ROUTER_CP_INTERFACE.get("address"),
-    ip_dst =  ALLSPFRouters):
-    #TODO tutaj dst nie all routers
-    p = scapy_ospf.Ether(src = eth_src, dst = eth_dst)/IP(src = ip_src, dst = ip_dst )/scapy_ospf.OSPF_Hdr(src=ROUTER_ID,area=AREA_ID)/scapy_ospf.OSPF_LSUpd()
-    return p
-    
-
 
 ###---------------------------------###
 ### ----------- RUNNING ----------- ###
